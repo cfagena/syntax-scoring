b7500a09ba3ceaa98238bf1dee99b05c017b8465
Cesar Agena <cesar.agena@cevt.se>
2022-08-27 15:30:01 +0200

    Implement algorithm and run example test

--- app/src/main/java/com/agena/android/syntaxscoring/MainViewModel.kt
+++ app/src/main/java/com/agena/android/syntaxscoring/MainViewModel.kt
@@ -1,10 +1,18 @@
 package com.agena.android.syntaxscoring
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import java.lang.Exception
+import java.util.EmptyStackException
+import java.util.Stack
 
 class MainViewModel : ViewModel() {
+
+    companion object {
+        private const val TAG: String = "MainViewModel"
+    }
 
     private val _input = MutableLiveData("")
     val input: LiveData<String> = _input
@@ -14,6 +22,117 @@ class MainViewModel : ViewModel() {
     }
 
     fun process() {
-        TODO("Not yet implemented")
+        val subsystem = input.value
+        var score = 0
+
+        subsystem?.let {
+            if (it.isNotBlank()) {
+                it.lines().forEachIndexed { index, line ->
+                    Log.d(TAG, "line $index: $line")
+                    val lineResult = processLine(line, index)
+                    if (LineResult.CORRUPTED == lineResult.first) {
+                        score += lineResult.second
+                    }
+                }
+            }
+        }
+        Log.d(TAG, "Total syntax error score: $score")
     }
+
+    private fun processLine(line: String, lineIndex: Int): Pair<LineResult, Int> {
+        if (line.isBlank())
+            return Pair(LineResult.COMPLETE, 0)
+
+        // Stack data structure (LIFO) to store the opening characters
+        val lineStack = Stack<Char>()
+
+        line.forEachIndexed { charIndex, character ->
+            when (classifyCharacter(character)) {
+                CharacterType.OPENING -> {
+                    // If opening character push to the stack
+                    lineStack.push(character)
+                }
+
+                CharacterType.CLOSING -> {
+                    // If it is a closing character check if it matches if last pushed character
+                    // on the stack
+                    try {
+                        val poppedChar = lineStack.pop()
+                        if (matchToOpeningCharacter(poppedChar, character)) {
+                            Log.d(TAG, "Matching characters: $poppedChar $character")
+                        } else {
+                            Log.d(
+                                TAG,
+                                "Corrupted line. Expected ${closingCharacter(poppedChar)}, " +
+                                    "but found $character instead at $lineIndex:$charIndex"
+                            )
+                            return Pair(LineResult.CORRUPTED, getPointsForCharacter(character))
+                        }
+                    } catch (e: EmptyStackException) {
+                        Log.d(
+                            TAG,
+                            "No opening character for: [$character, $lineIndex:$charIndex]"
+                        )
+                        return Pair(LineResult.CORRUPTED, getPointsForCharacter(character))
+                    }
+                }
+
+                CharacterType.INVALID -> {
+                    // If it is not expected character just ignoring it
+                    Log.w(
+                        TAG,
+                        "Character invalid: [$character, $lineIndex:$charIndex], ignoring it"
+                    )
+                }
+            }
+        }
+        if (!lineStack.empty())
+            Pair(LineResult.INCOMPLETE, 0)
+
+        return Pair(LineResult.COMPLETE, 0)
+    }
+
+    private fun classifyCharacter(character: Char): CharacterType {
+        return when (character) {
+            '(', '[', '{', '<' -> CharacterType.OPENING
+            ')', ']', '}', '>' -> CharacterType.CLOSING
+            else -> CharacterType.INVALID
+        }
+    }
+
+    private fun matchToOpeningCharacter(openingCharacter: Char, closingCharacter: Char): Boolean {
+        return closingCharacter(openingCharacter) == closingCharacter
+    }
+
+    private fun closingCharacter(openingCharacter: Char): Char {
+        return when (openingCharacter) {
+            '(' -> ')'
+            '[' -> ']'
+            '{' -> '}'
+            '<' -> '>'
+            else -> throw Exception("Invalid State, this character should not be here")
+        }
+    }
+
+    private fun getPointsForCharacter(character: Char): Int {
+        return when (character) {
+            ')' -> 3
+            ']' -> 57
+            '}' -> 1197
+            '>' -> 25137
+            else -> throw Exception("Invalid State, this character should not be here")
+        }
+    }
+}
+
+enum class LineResult {
+    CORRUPTED,
+    INCOMPLETE,
+    COMPLETE
+}
+
+enum class CharacterType {
+    OPENING,
+    CLOSING,
+    INVALID
 }
